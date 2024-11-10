a71aa51ea9cf850a7eecb2df8aa59a6e253d017c
xKristee29 <kykysoy@yahoo.com>
2024-11-10 16:37:11 +0200

    TeleOp finished for Lyra event, more needs to come

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpMecanum.java
@@ -2,11 +2,11 @@ package org.firstinspires.ftc.teamcode.testing;
 
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -22,55 +22,83 @@ public class TeleOpMecanum extends LinearOpMode {
     Servo servoGlisiera;
     CRServo servoIntake;
 
+    DcMotor glisiera1, glisiera2;
 
     boolean intakeExtended = false;
 
     Timing.Timer intakeTimer = new Timing.Timer(1, TimeUnit.MILLISECONDS);
     Timing.Timer intakeCooldown = new Timing.Timer(1, TimeUnit.MILLISECONDS);
 
-    int posEnb = 1600;
-    int threshold = 100;
+    Timing.Timer intakeIn = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+    Timing.Timer intakeOut = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+
+    int posEnb = 280;
+    int threshold = 20;
 
     public void intakeEnb(){
 
-        if(gamepad2.a && !intakeExtended && intakeCooldown.done()){
+        if(gamepad1.a && !intakeExtended && intakeCooldown.done()){
 
             intakeExtended = true;
             motorGlisiera.setTargetPosition(posEnb);
-            motorGlisiera.setPower(1);
             motorGlisiera.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorGlisiera.setPower(1);
+            servoIntake.setPower(1);
 
-            intakeCooldown = new Timing.Timer(1, TimeUnit.MILLISECONDS);
             intakeTimer = new Timing.Timer(600, TimeUnit.MILLISECONDS);
+            intakeTimer.start();
 
-        } else if (gamepad2.a && intakeExtended && intakeCooldown.done()) {
+            intakeCooldown = new Timing.Timer(200, TimeUnit.MILLISECONDS);
+            intakeCooldown.start();
+
+        } else if (gamepad1.a && intakeExtended && intakeCooldown.done()) {
 
             intakeExtended = false;
             motorGlisiera.setTargetPosition(0);
-            motorGlisiera.setPower(1);
             motorGlisiera.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorGlisiera.setPower(1);
 
-            intakeCooldown = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+            servoIntake.setPower(0);
+
             intakeTimer = new Timing.Timer(600, TimeUnit.MILLISECONDS);
+            intakeTimer.start();
+
+            intakeCooldown = new Timing.Timer(200, TimeUnit.MILLISECONDS);
+            intakeCooldown.start();
         }
 
         if(intakeExtended &&
                 Math.abs(motorGlisiera.getCurrentPosition() - posEnb) <= threshold){
-            servoGlisiera.setPosition(1);
+            servoGlisiera.setPosition(0.4);
         }
 
         if(!intakeExtended){
-            servoGlisiera.setPosition(0.5);
+            servoGlisiera.setPosition(0);
         }
 
     }
 
     public void intakeMove(double lt, double rt){
-        if (motorGlisiera.getCurrentPosition() - posEnb > -100 && intakeTimer.done()) {
+        if (rt - lt != 0 && intakeTimer.done()) {
             motorGlisiera.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             motorGlisiera.setPower(rt-lt);
         }
 
+    }
+
+    public void intakeToggle(){
+        if(gamepad1.x && intakeIn.done()){
+            servoIntake.setPower(1-servoIntake.getPower());
+
+            intakeIn = new Timing.Timer(200, TimeUnit.MILLISECONDS);
+            intakeIn.start();
+        }
+        if(gamepad1.b && intakeOut.done()){
+            servoIntake.setPower(-(1-Math.abs(servoIntake.getPower())));
+
+            intakeOut = new Timing.Timer(200, TimeUnit.MILLISECONDS);
+            intakeOut.start();
+        }
     }
 
     @Override
@@ -83,12 +111,32 @@ public class TeleOpMecanum extends LinearOpMode {
             rf = new Motor(hardwareMap, "rightFront");
             rr = new Motor(hardwareMap, "rightRear");
 
+            servoGlisiera = hardwareMap.servo.get("servoGlisiera");
+            servoIntake = hardwareMap.crservo.get("servoIntake");
+
+            glisiera1 = hardwareMap.dcMotor.get("glisiera1");
+            glisiera2 = hardwareMap.dcMotor.get("glisiera2");
+
+            motorGlisiera = hardwareMap.dcMotor.get("motorGlisiera");
             motorGlisiera.setPower(0);
+            motorGlisiera.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motorGlisiera.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             motorGlisiera.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
             drive = new MecanumDrive(lf, rf, lr, rr);
             driverOp = new GamepadEx(gamepad1);
+
+            intakeTimer = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+            intakeTimer.start();
+
+            intakeCooldown = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+            intakeCooldown.start();
+
+            intakeIn = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+            intakeIn.start();
+
+            intakeOut = new Timing.Timer(1, TimeUnit.MILLISECONDS);
+            intakeOut.start();
 
             waitForStart();
 
@@ -103,9 +151,24 @@ public class TeleOpMecanum extends LinearOpMode {
 
                 intakeEnb();
 
-               intakeMove(gamepad2.left_trigger,gamepad2.right_trigger);
+                intakeMove(gamepad1.left_trigger,gamepad1.right_trigger);
 
+                intakeToggle();
 
+                //glisiera1.setPower(-gamepad1.left_stick_y);
+                if(gamepad1.dpad_up) glisiera1.setPower(-0.5);
+                else if(gamepad1.dpad_down) glisiera1.setPower(0.5);
+                else glisiera1.setPower(0);
+
+                telemetry.addData("Pos",motorGlisiera.getCurrentPosition());
+                telemetry.addData("Ext",intakeExtended);
+                telemetry.addData("Cooldown",intakeCooldown.done());
+                telemetry.addData("Timer",intakeTimer.done());
+                telemetry.addData("In",intakeIn.done());
+                telemetry.addData("Out",intakeOut.done());
+                telemetry.addData("G1",glisiera1.getCurrentPosition());
+                telemetry.addData("G2",glisiera2.getCurrentPosition());
+                telemetry.update();
             }
 
             throw new InterruptedException();
